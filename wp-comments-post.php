49ea1e97801e723dcbca1b1f322c576126b884bc
STAR7077 <joshuaskitchen91@gmail.com>
2025-06-14 09:48:28 -0400

    Update wp-comments-post.php

--- wp-comments-post.php
+++ wp-comments-post.php
@@ -10,7 +10,7 @@ if ( 'POST' !== $_SERVER['REQUEST_METHOD'] ) {
 	if ( ! in_array( $protocol, array( 'HTTP/1.1', 'HTTP/2', 'HTTP/2.0', 'HTTP/3' ), true ) ) {
 		$protocol = 'HTTP/1.0';
 	}
-
+   
 	header( 'Allow: POST' );
 	header( "$protocol 405 Method Not Allowed" );
 	header( 'Content-Type: text/plain' );
